43447d15147cdb5f1f7d93a05bed3e7c43b4fb97
Ilya Desyatov <appleilyadesyatov@gmail.com>
2024-08-21 04:24:40 +0300

    HW_17

---
+++ WB_Chat/WB_Chat/WB_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WB_ChatApp.swift
+//  WB_Chat
+//
+//  Created by Илья Десятов on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WB_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
